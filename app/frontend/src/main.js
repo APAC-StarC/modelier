ab5e1ed5d6a6f406c1eaf141e2f43524f68d9a43
Kyriakos Toumbas <kyriakos@jentamin.com>
2021-06-15 10:46:23 +0300

    Cleanup

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import AdminApp from './AdminApp.vue'
+import AdminApp from './App.vue'
 import VueCompositionAPI from '@vue/composition-api'
 Vue.use(VueCompositionAPI)
 
@@ -70,68 +70,3 @@ if (menuConf.toolsEnabled){
 
     });
 }
-
-
-/*
-<template>
-  <div>
-    <Slider v-model="value" />
-  </div>
-</template>
-
-<script>
-  import Slider from '@vueform/slider/dist/slider.vue2.js'
-
-  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
-  export default {
-    components: {
-      Slider,
-    },
-    data() {
-      return {
-        value: 20
-      }
-    }
-  }
-</script>
-
-<style src="@vueform/slider/themes/default.css"></style>
-
-<template>
-  <div>
-    <Toggle v-model="value" />
-  </div>
-</template>
-
-<script>
-  import Toggle from '@vueform/toggle/dist/toggle.vue2.js'
-
-  export default {
-    components: {
-      Toggle,
-    },
-    data() {
-      return {
-        value: true
-      }
-    }
-  }
-</script>
-
-<style src="@vueform/toggle/themes/default.css"></style>
-<Toggle v-model="value" :classes="{
-  container: 'inline-block',
-  toggle: 'flex w-12 h-5 rounded-full relative cursor-pointer transition items-center box-content border-2 text-xs leading-none',
-  toggleOn: 'bg-green-500 border-green-500 justify-start text-white',
-  toggleOff: 'bg-gray-200 border-gray-200 justify-end text-gray-700',
-  toggleOnDisabled: 'bg-gray-300 border-gray-300 justify-start text-gray-400 cursor-not-allowed',
-  toggleOffDisabled: 'bg-gray-200 border-gray-200 justify-end text-gray-400 cursor-not-allowed',
-  handle: 'inline-block bg-white w-5 h-5 top-0 rounded-full absolute transition-all',
-  handleOn: 'left-full transform -translate-x-full',
-  handleOff: 'left-0',
-  handleOnDisabled: 'bg-gray-100 left-full transform -translate-x-full',
-  handleOffDisabled: 'bg-gray-100 left-0',
-  label: 'text-center w-8 border-box whitespace-nowrap select-none',
-}" />
- */
