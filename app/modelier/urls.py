efecead54f99d7ee7f3fad6ebc0424c62b65a0c4
Kyriakos Toumbas <kyriakos@jentamin.com>
2021-10-06 10:11:27 +0300

    Update admin title to say IHAT

--- app/modelier/urls.py
+++ app/modelier/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                             document_root=settings.MEDIA_ROOT)
-admin.site.site_header = "Modelier"
+admin.site.site_header = "IH-AT"
