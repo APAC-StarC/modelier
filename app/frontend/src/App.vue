aab1eb278218631cbccef7c9b61f02c62a156c4d
Kyriakos Toumbas <kyriakos@jentamin.com>
2021-11-28 19:03:05 +0200

    Inject vue css

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -568,14 +568,4 @@ export default {
 }
 </script>
 
-<style>
-#admin-app {
-  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
-}
-</style>
 <style src="@vueform/toggle/themes/default.css"></style>
